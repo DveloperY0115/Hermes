dcafa8764f51125df520a3212ad110094dcf8125
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2021-09-16 00:57:35 +0900

    Create model files

---
+++ Vision/models/EfficientNet/efficientnet.py
@@ -0,0 +1,4 @@
+"""
+efficientnet.py - Pytorch implementation of "EfficientNet: Rethinking Model Scaling for Convolutional Neural Networks, Tan et al., (ICML 2019)"
+"""
+
---
+++ Vision/models/RegNet/regnet.py
@@ -0,0 +1,4 @@
+"""
+regnet.py - Pytorch implementation of "Designing Network Design Spaces, Radosavovic et al., (CVPR 2020)"
+"""
+
