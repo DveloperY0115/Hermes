90ec396e60104b4ed2f1923c1faf2546a4fcf2af
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2021-09-19 18:16:14 +0900

    Add HermesEfficientNet model

--- Vision/models/EfficientNet/efficientnet.py
+++ Vision/models/EfficientNet/efficientnet.py
@@ -2,3 +2,52 @@
 efficientnet.py - Pytorch implementation of "EfficientNet: Rethinking Model Scaling for Convolutional Neural Networks, Tan et al., (ICML 2019)"
 """
 
+import torch
+import torch.optim as optim
+
+import pycls.models as models
+import pytorch_lightning as pl
+from torchsummary import summary
+
+
+class HermesEfficientNet(pl.LightningModule):
+    def __init__(self, type: str, load_pretrained: bool = True, verbose=False):
+        """
+        Construct EfficientNet model by loading from pycls' model zoo.
+
+        Args:
+        - type: Type of EfficientNet model to be loaded.
+        - load_pretrained: Determine whether to download pretrained weights. Set to true by default.
+        - verbose: Determine whether to report all progress or not. Set to false by default, keeping initialization silent.
+        """
+        super().__init__()
+
+        # initialize EfficientNet
+        # For details, please refer to https://github.com/facebookresearch/pycls
+        self.network = models.effnet(type, pretrained=load_pretrained).cuda()
+
+        if verbose:
+            print("[!] Successfully loaded EfficientNet-" + type)
+
+            # print model summary
+            summary(self.network, input_size=(3, 224, 224), device="cuda")
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward propagation.
+
+        Args:
+        - x: A tensor of shape (*, C, H, W) representing a batch of images. Typically W = H = 224.
+        """
+        out = self.network(x)
+        return out
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def training_step(self, train_batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
+        raise NotImplementedError
+
+    def validation_step(self, val_batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
+        raise NotImplementedError
---
+++ Vision/models/EfficientNet/scripts/train.py
@@ -0,0 +1,33 @@
+"""
+train.py - Training script for HermesEfficientNet models
+"""
+
+import sys
+import torch.utils.data as data
+from torch.utils.data import dataset
+import torchvision.datasets as datasets
+import pytorch_lightning as pl
+
+sys.path.append(".")
+sys.path.append("..")
+
+from Vision.models.EfficientNet.efficientnet import HermesEfficientNet
+
+
+def main():
+
+    # initialize model
+    effnet = HermesEfficientNet("B2", verbose=True)
+
+    # prepare datasets
+    # train_dataset, valid_dataset = get_imagenet_dataset()
+
+    # train_loader = data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+    # valid_loader = data.DataLoader(valid_dataset, batch_size=32, shuffle=False)
+
+    # trainer = pl.Trainer(gpus=[0], precision=16, limit_train_batches=0.5)
+    # trainer.fit(model, train_loader, valid_loader)
+
+
+if __name__ == "__main__":
+    main()
