7ed5f2533eb2582c7cec00457a4b7202a81ff3fc
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2021-09-19 17:36:19 +0900

    Replace model zoo of torchvision with pycls'

--- Vision/models/ResNet/resnet.py
+++ Vision/models/ResNet/resnet.py
@@ -3,36 +3,29 @@ resnet.py - Pytorch implementation of "Deep Residual Learning for Image Recognit
 """
 
 import torch
-from torch._C import Value
-from torch.nn.functional import triplet_margin_loss
-
 import torch.optim as optim
-import torchvision.models as models
+
+import pycls.models as models
 import pytorch_lightning as pl
-from torchvision.models.resnet import resnet34
 
 
 class HermesResNet(pl.LightningModule):
-    def __init__(self, resnet_depth: int = 34, load_pretrained: bool = True):
+    def __init__(self, resnet_depth: int = 50, load_pretrained: bool = True):
         """
         Construct ResNet model by loading from torchvision's ResNet model zoo.
 
         Args:
-        - resnet_depth: A number of hidden layers in the model. Can be one of 18, 34, 50, 101, 152. Set to 34 by default.
+        - resnet_depth: A number of hidden layers in the model. Can be one of 50, 101, 152. Set to 50 by default.
         - load_pretrained: Determine whether to download pretrained weights. Set to true by default.
         """
         super().__init__()
 
-        if resnet_depth == 18:
-            self.resnet = models.resnet18(pretrained=load_pretrained, progress=True)
-        elif resnet_depth == 34:
-            self.resnet = models.resnet34(pretrained=load_pretrained, progress=True)
-        elif resnet_depth == 50:
-            self.resnet = models.resnet50(pretrained=load_pretrained, progress=True)
+        if resnet_depth == 50:
+            self.resnet = models.resnet("50", pretrained=load_pretrained)
         elif resnet_depth == 101:
-            self.resnet = models.resnet101(pretrained=load_pretrained, progress=True)
+            self.resnet = models.resnet("101", pretrained=load_pretrained)
         elif resnet_depth == 152:
-            self.resnet = models.resnet152(pretrained=load_pretrained, progress=True)
+            self.resnet = models.resnet("152", pretrained=load_pretrained)
         else:
             raise ValueError("[!] Invalid number of hidden layers.")
 
@@ -43,7 +36,7 @@ class HermesResNet(pl.LightningModule):
         Args:
         - x: A tensor of shape (*, C, H, W) representing a batch of images. Typically W = H = 224.
         """
-        out = self.resnet34(x)
+        out = self.resnet(x)
         return out
 
     def configure_optimizers(self):
