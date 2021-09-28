49528d71519e385910536449a5c2f4ee31dc8f09
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2021-09-29 00:33:43 +0900

    Visualize RegNetY using Tensorboard

--- Vision/models/HydraNet/Backbone/RegNet/scripts/train.py
+++ Vision/models/HydraNet/Backbone/RegNet/scripts/train.py
@@ -4,23 +4,39 @@ train.py - Training script for HermesRegNet models
 
 import os
 import sys
+import torch
 import torch.utils.data as data
 from torch.utils.data import dataset
+from torch.utils.tensorboard import SummaryWriter
 import torchvision.datasets as datasets
 import pytorch_lightning as pl
 
 sys.path.append(".")
 sys.path.append("..")
 
-from Vision.models.RegNet.regnetX import HermesRegNetX
-from Vision.models.RegNet.regnetY import HermesRegNetY
+from Vision.models.HydraNet.Backbone.RegNet.regnetX import HermesRegNetX
+from Vision.models.HydraNet.Backbone.RegNet.regnetY import HermesRegNetY
 
 
 def main():
 
+    # initialize summary writer for model visualization
+    log_dir = "Vision/models/HydraNet/Backbone/RegNet/scripts/logs"
+
+    os.makedirs(log_dir, exist_ok=True)
+
+    writer = SummaryWriter(log_dir)
+
     # initialize model
-    regnetX = HermesRegNetX("400MF", verbose=True)
-    regnetY = HermesRegNetY("400MF", verbose=True)
+    regnetX = HermesRegNetX()
+    regnetY = HermesRegNetY()
+
+    _ = regnetY.get_stage_features(torch.zeros((1, 3, 224, 224)).cuda())
+
+    # draw computational graph(s)
+    writer.add_graph(regnetX, torch.zeros(1, 3, 224, 224).cuda())
+    writer.add_graph(regnetY, torch.zeros(1, 3, 224, 224).cuda())
+    writer.close()
 
     # prepare datasets
     # train_dataset, valid_dataset = get_imagenet_dataset()
